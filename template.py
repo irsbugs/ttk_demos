79b8573bd6d703392a1a1a3f6e4760abc1631d68
irsbugs <irsbugs@hotmail.com>
2016-02-03 13:39:05 +1300

    Create template.py

---
+++ template.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# Program description comments
+import modules
+CONSTANTS = define
+variables = assign values
+class GUI():
+    def init GUI and setup widgets
+        create style templates 
+        create the widgets
+        set the grid geometry
+        apply default values when launching program
+    def callbacks from widgets
+    def processing within the GUI class
+def processing outside of the GUI class
+if __name__ == "__main__":
+    sys.argv[1] check for options
+        help information
+        utility to call module to copy to /usr/local/bin/ 
+    call GUI class
+    loop()
+"""
+Notes: About the program and references.
+"""
